d70d139f10a29c0e2bb447c05758b16614e5f959
ANA SCHMIDT <sfca.cris@gmail.com>
2024-09-01 21:54:53 -0300

    Projeto desenvolvido no primeiro curso da formação Java da Alura.

---
+++ src/Soma.java
@@ -0,0 +1,9 @@
+public class Soma {
+    public static void main(String[] args) {
+
+        int somaA = 10;
+        int somaB = 5;
+        int resultado = somaA + somaB;
+    System.out.println("resultado = " + resultado);
+    }
+}
---
+++ src/Subtracao.java
@@ -0,0 +1,9 @@
+public class Subtracao {
+    public static void main(String[] args) {
+        int Subtracao1 = 10;
+        int Subtracao2 = 5;
+
+        int Resultado = Subtracao1 - Subtracao2;
+      System.out.println("Resultado = " + Resultado);
+        }
+}
